a974f88568eef8525837da07ef42d441a1604de0
Carlos Rodriguez <Vanilla6644@gmail.com>
2022-11-06 11:13:43 -0700

    done get player choice

--- javascript.js
+++ javascript.js
@@ -1,6 +1,10 @@
+const string1 = "rock";
+const string2 = "paper";
+const string3 = "scissors";
+
 //get computer choice
 function getComputerChoice(params) {
-  let randNum = Math.floor(Math.random() * 3) + 1;
+  let randNum = Math.floor(Math.random() * 1) + 1;
   if (randNum === 1) {
     const string1 = "rock";
     console.log(string1);
@@ -15,9 +19,24 @@ function getComputerChoice(params) {
 getComputerChoice();
 
 //get player choice
-function playerSelection() {
-  let player = prompt("choose rock,paper or scissors").toLowerCase;
-
-  console.log(player);
+function playerchoise() {
+  let player = prompt("choose rock,paper or scissors");
+  if (player.toLowerCase() === string1.toLowerCase()) {
+    console.log(string1);
+  } else if (player.toLowerCase() === string2.toLowerCase()) {
+    console.log(string2);
+  } else if (player.toLowerCase() === string3.toLowerCase()) {
+    console.log(string3);
+  } else {
+    console.log("please try again");
+  }
 }
-playerSelection();
+
+playerchoise();
+
+function Round(computerSelection, playerSelection) {
+  if ((computerSelection === 1) & (playerSelection === string1.toLowerCase)) {
+    console.log("hi");
+  }
+}
+console.log(Round(playerchoise, getComputerChoice));
