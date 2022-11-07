d29f151fb592530b6a959945b473cdf718473bee
Carlos Rodriguez <Vanilla6644@gmail.com>
2022-11-06 23:43:50 -0700

    add play1round

--- javascript.js
+++ javascript.js
@@ -1,42 +1,68 @@
 const string1 = "rock";
 const string2 = "paper";
 const string3 = "scissors";
-
-//get computer choice
-function getComputerChoice(params) {
-  let randNum = Math.floor(Math.random() * 1) + 1;
-  if (randNum === 1) {
-    const string1 = "rock";
-    console.log(string1);
-  } else if (randNum === 2) {
-    const string2 = "paper";
-    console.log(string2);
-  } else {
-    const string3 = "scissors";
-    console.log(string3);
-  }
-}
-getComputerChoice();
+let computerSelection = 0;
+let playerSelection = 0;
 
 //get player choice
-function playerchoise() {
+function playerchoice() {
   let player = prompt("choose rock,paper or scissors");
   if (player.toLowerCase() === string1.toLowerCase()) {
     console.log(string1);
+    playerSelection = 1;
   } else if (player.toLowerCase() === string2.toLowerCase()) {
+    playerSelection = 2;
     console.log(string2);
   } else if (player.toLowerCase() === string3.toLowerCase()) {
+    playerSelection = 3;
     console.log(string3);
   } else {
     console.log("please try again");
   }
 }
 
-playerchoise();
-
-function Round(computerSelection, playerSelection) {
-  if ((computerSelection === 1) & (playerSelection === string1.toLowerCase)) {
-    console.log("hi");
+playerchoice();
+//get computer choice
+function getComputerChoice(params) {
+  let randNum = Math.floor(Math.random() * 3) + 1;
+  if (randNum === 1) {
+    computerSelection = 1;
+    console.log(string1);
+  } else if (randNum === 2) {
+    computerSelection = 2;
+    console.log(string2);
+  } else {
+    computerSelection = 3;
+    console.log(string3);
   }
 }
-console.log(Round(playerchoise, getComputerChoice));
+
+getComputerChoice();
+
+function playRound(computerSelection, playerSelection) {
+  if ((playerSelection === 2) & (computerSelection === 1)) {
+    //player win
+    console.log("paper beats rock you win");
+  } else if ((playerSelection === 3) & (computerSelection === 2)) {
+    console.log("scissors beats paper you win");
+  } else if ((playerSelection === 1) & (computerSelection === 3)) {
+    console.log("rock beats scissors you win");
+    //computer win
+  } else if ((computerSelection === 2) & (playerSelection === 1)) {
+    console.log("paper beats rock you lose");
+  } else if ((computerSelection === 3) & (playerSelection === 2)) {
+    console.log("scissors beats paper you lose");
+  } else if ((computerSelection === 1) & (playerSelection === 3)) {
+    console.log("rock beats scissors you lose");
+  }
+  //ties
+  else if ((computerSelection === 1) & (playerSelection === 1)) {
+    console.log("tie");
+  } else if ((computerSelection === 2) & (playerSelection === 2)) {
+    console.log("tie");
+  } else if ((computerSelection === 3) & (playerSelection === 3)) {
+    console.log("tie");
+  }
+}
+
+playRound(computerSelection, playerSelection);
